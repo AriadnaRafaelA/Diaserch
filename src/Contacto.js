2fb9db172278587e845d29cd74d99293819001e9
AriadnaRafaelA <ariadnarafael@gmail.com>
2023-11-25 00:53:58 -0600

    cambios

--- src/Contacto.js
+++ src/Contacto.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
 function Contacto(props) {
@@ -51,41 +51,85 @@ function Contacto(props) {
     textAlign: 'center',
   };
 
+  const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const R = 6371; // Radio de la Tierra en kilómetros
+    const dLat = (lat2 - lat1) * (Math.PI / 180);
+    const dLon = (lon2 - lon1) * (Math.PI / 180);
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos(lat1 * (Math.PI / 180)) * Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distancia en kilómetros
+    return distance.toFixed(2); // Redondear a 2 decimales
+  };
+
+  const [userLocation, setUserLocation] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setUserLocation({ lat: latitude, lng: longitude });
+      }, (error) => {
+        console.error(`Error getting user location: ${error.message}`);
+      });
+    } else {
+      console.error('Geolocation is not supported by this browser.');
+    }
+  }, []);
+
   return (
     <div>
-      {mapData.map((data, index) => (
-        <div key={index} style={containerStyles}>
-             <h2 style={titleStyles}>{data.title}</h2>
-             <div style={infoStyles}>
+      {mapData.map((data, index) => {
+        const distance = userLocation ? calculateDistance(userLocation.lat, userLocation.lng, data.lat, data.lng) : null;
+        return (
+          <div key={index} style={containerStyles}>
+            <h2 style={titleStyles}>{data.title}</h2>
+            <div style={infoStyles}>
               <p>
                 Página web: <a href={data.website}>{data.website}</a>
                 <br />
                 Teléfono: {data.telefono}
+                <br />
+                {userLocation && <span>Distancia: {distance} km</span>}
               </p>
             </div>
-          <div style={mapStyles}>
-            <Map
-              google={props.google}
-              zoom={14}
-              initialCenter={{
-                lat: data.lat,
-                lng: data.lng,
-              }}
-            >
-              <Marker
-                position={{
+            <div style={mapStyles}>
+              <Map
+                google={props.google}
+                zoom={14}
+                initialCenter={{
                   lat: data.lat,
                   lng: data.lng,
                 }}
-              />
-            </Map>
+              >
+                <Marker
+                  position={{
+                    lat: data.lat,
+                    lng: data.lng,
+                  }}
+                />
+                {userLocation && (
+                  <Marker
+                    position={{
+                      lat: userLocation.lat,
+                      lng: userLocation.lng,
+                    }}
+                    icon={{
+                      url: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                    }}
+                  />
+                )}
+              </Map>
+            </div>
           </div>
-         
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
+
 
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyCCgPfn4jH9TEz7dqVu_P1SQyh2KBgJbKU', // Tu clave de API
