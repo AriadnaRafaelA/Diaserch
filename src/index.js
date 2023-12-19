b4e4629824adc9ee9e21fb19f72ad886db87cff2
Humberto Alcocer <humbertowoody@gmail.com>
2023-12-18 19:02:43 -0600

    fix(react): dom version

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Importa el CSS de Bootstrap
-import 'bootstrap/dist/js/bootstrap.bundle.min'; // Importa el JavaScript de Bootstrap
-import './index.css';
-import App from './App';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css"; // Importa el CSS de Bootstrap
+import "bootstrap/dist/js/bootstrap.bundle.min"; // Importa el JavaScript de Bootstrap
+import "./index.css";
+import App from "./App";
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-function MyApp() {
-  useEffect(() => {
-    document.title = 'DIASERCH'; // Cambia 'Nuevo Título' por el título que desees
-  }, []);
-
-  return (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<MyApp />);
-
-/*const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);*/
-
-
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
